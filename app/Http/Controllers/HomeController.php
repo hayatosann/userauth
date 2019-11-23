bf0f9fc4472776dff34f533a0b9876c2c8be2de8
hayatosann <sasano.hayato@icloud.com>
2019-11-24 01:53:53 +0900

    write down paginate

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::paginate(1);
         return view('home',['users' => $users]);
     }
 }
