cb08deadcd518889fdf6d2f8b6b4b082d606d822
hayatosann <sasano.hayato@icloud.com>
2019-11-24 01:30:22 +0900

    Input user sample

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,5 +12,17 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
+        $users = [
+            ["name"=>"test1","email"=>"test1@test.com","password"=>"(uhBnji9"],
+            ["name"=>"test2","email"=>"test2@test.com","password"=>"(uhBnji9"],
+            ["name"=>"test3","email"=>"test3@test.com","password"=>"(uhBnji9"]
+            ];
+        foreach($users as $user) {
+            $u = new User;
+            $u->name = $user["name"];
+            $u->email = $user["email"];
+            $u->password = bcrypt($user["password"]);
+            $u->save();
+            }
     }
 }
