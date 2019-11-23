ba3bf27c21d6483cac699c3d84800addd5a0d57f
hayatosann <sasano.hayato@icloud.com>
2019-11-24 01:38:49 +0900

    add user information in index action

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $users = User::all();
+        return view('home',['users' => $users]);
     }
 }
